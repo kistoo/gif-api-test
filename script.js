a8d7835c7c43b8e5a25c164be574118b64c2e776
Tomio Nakamurakare <112982995+kistoo@users.noreply.github.com>
2022-12-27 20:10:55 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const button = document.querySelector('button');
 const img = document.querySelector('img');
+const input = document.querySelector('input');
+const errorMessage = document.querySelectorAll('h1')[1];
 
 button.onclick = function () {
-  fetch('https://api.giphy.com/v1/gifs/translate?api_key=QhRNw4GcRcA8QmIQjZxlGWThYUKCg4tx&s=cats', {mode: 'cors'})
+  errorMessage.style.display = 'none';
+  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=QhRNw4GcRcA8QmIQjZxlGWThYUKCg4tx&s=${input.value}`, {mode: 'cors'})
     .then(function(response) {
       return response.json();
     })
     .then(function(response) {
       img.src = response.data.images.original.url;
-    });
+    })
+    .catch((error) => {
+      errorMessage.style.display = 'block';
+      img.src = '';
+      console.log("dsd")
+    })
 }
     
